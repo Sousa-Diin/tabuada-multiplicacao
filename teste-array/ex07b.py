edc73f35d7a04f8b8916ead76fdf44e4ef91e60c
Sousa-Diin <74382948+Sousa-Diin@users.noreply.github.com>
2021-05-29 16:33:21 -0300

    criei o arquivo principal

---
+++ teste-array/ex07b.py
@@ -0,0 +1,7 @@
+print("_______________TABUADA MULTIPLICAÇÃO______________")
+print('example: tabuada [1] | tabuada [2] ....')
+tabu = int(input('Digite qual tabuada deseja exibir: '))
+
+mult_tabu = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in mult_tabu:
+    print(tabu, 'X', mult_tabu[i], ': {}'.format(tabu * mult_tabu[i]))
